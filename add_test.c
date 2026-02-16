0f6b71165e939348e78017bb0417b7defe62702e
Ivan Sharun <ivan@sha.run>
2026-02-16 13:02:26 +0600

    feat: ci

--- add_test.c
+++ add_test.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 
 #include "add.h"
 
@@ -11,11 +12,25 @@ void test_add_int_1_2() {
     int res = add_int(a, b);
 
     // Assert
-    assert(res == 4);
+    assert(res == 3);
+}
+
+void test_add_double_general() {
+    // Arrange
+    double a = 0.1;
+    double b = 0.2;
+    
+    // Act
+    double res = add_double(a, b);
+    // double eps = 1E-8;
+    // Assert
+    // assert(fabs(res - 0.3) < eps);
+    assert(res == 0.3);
 }
 
 
 int main() {
     test_add_int_1_2();
+    test_add_double_general();
     return 0;
 }
