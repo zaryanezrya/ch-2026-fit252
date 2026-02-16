20486d28380ba0e9464e6c5f1df99c50e7a8d738
Ivan Sharun <ivan@sha.run>
2026-02-16 13:04:14 +0600

    chore: formatting

--- add_test.c
+++ add_test.c
@@ -4,33 +4,32 @@
 #include "add.h"
 
 void test_add_int_1_2() {
-    // Arrange
-    int a = 1;
-    int b = 2;
-    
-    // Act
-    int res = add_int(a, b);
+  // Arrange
+  int a = 1;
+  int b = 2;
 
-    // Assert
-    assert(res == 3);
+  // Act
+  int res = add_int(a, b);
+
+  // Assert
+  assert(res == 3);
 }
 
 void test_add_double_general() {
-    // Arrange
-    double a = 0.1;
-    double b = 0.2;
-    
-    // Act
-    double res = add_double(a, b);
-    // double eps = 1E-8;
-    // Assert
-    // assert(fabs(res - 0.3) < eps);
-    assert(res == 0.3);
+  // Arrange
+  double a = 0.1;
+  double b = 0.2;
+
+  // Act
+  double res = add_double(a, b);
+  // double eps = 1E-8;
+  // Assert
+  // assert(fabs(res - 0.3) < eps);
+  assert(res == 0.3);
 }
 
-
 int main() {
-    test_add_int_1_2();
-    test_add_double_general();
-    return 0;
+  test_add_int_1_2();
+  test_add_double_general();
+  return 0;
 }
